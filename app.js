2482b92653b680454cdad14679e5bf84188eecbc
Technical Provider <infinite.reality.solution@gmail.com>
2021-05-09 17:56:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const streams = [
-  'http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8',
+  'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
   'https://vusr.wemersive.com/arctic/dash.mpd',
   'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd'
 ];
